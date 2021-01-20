14c3bea1ed35c7bc227a7cb4f19b4f44b9b9d312
faizainulikram@gmail.com <faizainulikram@gmail.com>
2021-01-20 16:03:43 +0800

    Payroll Bank Branch

---
+++ BIA.Dashboard.Template/Migrations/20210118004310_Mig77779.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BIA.Dashboard.Template.Migrations
+{
+    public partial class Mig77779 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PayrollAdvice_PayrollLedger_PayrollLedgerId",
+                table: "PayrollAdvice");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PayrollLedgerId",
+                table: "PayrollAdvice",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PayrollAdvice_PayrollLedger_PayrollLedgerId",
+                table: "PayrollAdvice",
+                column: "PayrollLedgerId",
+                principalTable: "PayrollLedger",
+                principalColumn: "PayrollLedgerId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PayrollAdvice_PayrollLedger_PayrollLedgerId",
+                table: "PayrollAdvice");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PayrollLedgerId",
+                table: "PayrollAdvice",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PayrollAdvice_PayrollLedger_PayrollLedgerId",
+                table: "PayrollAdvice",
+                column: "PayrollLedgerId",
+                principalTable: "PayrollLedger",
+                principalColumn: "PayrollLedgerId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
